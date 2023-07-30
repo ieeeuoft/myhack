a5055eb191bee08d3490ebb89a739c7709e0f0ea
KCui0327 <cuikenny9@gmail.com>
2023-07-29 23:03:19 -0700

    1: Create Dataloader and Dataset for ML Research
    - Created a web-scraper for data collection
    - Dataset and dataloader structure completed
    - metadata are generated as xlsx files
    
    Signed-off by: Kenny Cui <cuikenny9@gmail.com>

--- data_module/utils/dataset.py
+++ data_module/utils/dataset.py
@@ -1,7 +1,13 @@
 import os
 import pandas as pd
+<<<<<<< HEAD
 from torchvision.io import read_image
 from torch.utils.data import Dataset
+=======
+import pytorch_lightning as pl
+from torchvision.io import read_image
+from torch.utils.data import Dataset, DataLoader
+>>>>>>> 1fd36c5a (1: Create Dataloader and Dataset for ML Research)
 
 class MyHackImageDataset(Dataset):
     def __init__(self, metadata_csv, img_dir, category, transform=None):
@@ -19,3 +25,22 @@ class MyHackImageDataset(Dataset):
         if self.transform: image = self.transform(image)
             
         return image, label
+
+class MyHackDataModule(pl.LightningDataModule):
+    def __init__(self, metadata_csv, img_dir, category, transform=None, batch_size=32):
+        super().__init__()
+        self.metadata_csv = metadata_csv
+        self.img_dir = img_dir
+        self.category = category
+        self.transform = transform
+        self.batch_size = batch_size
+
+    def setup(self, stage=None):
+        # Initialize the datasets for training and validation
+        self.train_dataset = MyHackImageDataset(self.metadata_csv, self.img_dir, self.category, transform=self.transform)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
+    
+    def test_dataloader(self):
+        return DataLoader(test_data, batch_size=self.batch_size, shuffle=True)
