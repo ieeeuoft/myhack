3eafcc85a6d9c6eaf0c4384b6c848e0db9ba1f2b
HarryTheCoder-L <69062529+HarryTheCoder-L@users.noreply.github.com>
2023-11-14 11:33:46 -0500

    skeleton of auth UI and integration

---
+++ app/lib/features/authentication/wrapper.dart
@@ -0,0 +1,43 @@
+import 'package:app/features/home/home_page.dart';
+import 'package:app/features/profile/profile_page.dart';
+import 'package:app/features/team/team_page.dart';
+import 'package:flutter/material.dart';
+import 'package:app/features/authentication/widgets/google_sign_in_button.dart';
+
+class Wrapper extends StatelessWidget {
+  final Widget? child;
+  final bool switchBetween = false;
+  const Wrapper({super.key, this.child});
+  @override
+  Widget build(BuildContext context) {
+    if(switchBetween)
+      return child??Placeholder();
+    return Stack(
+      children: [
+        child??Placeholder(),
+        SizedBox.expand(
+          child: Container(
+              color: Colors.black.withOpacity(0.7),
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 200,
+                width : MediaQuery.of(context).size.width,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(50)
+                  ),
+                  color: Colors.white,
+                ),
+                child: const Column(
+                  children: [
+                    Text("To Access This Page:",style: TextStyle(fontSize: 10),),
+                    GoogleSignInButton()
+                  ],
+                )
+              ),
+          ),
+        ),
+      ],
+    );
+  }
+}
