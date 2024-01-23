2e6686897d3aac2da3af9b8b1ab65a6d27293362
Aaron Gu <gu20041006@gmail.com>
2024-01-23 17:22:59 -0500

    Merge previous work into new signin page

--- app/lib/features/profile/profile_page.dart
+++ app/lib/features/profile/profile_page.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import '../../app/global_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import 'package:app/features/profile/profile_widget.dart';
+import 'edit_profile.dart';
+
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
   static const String route = '/profile';
@@ -105,6 +108,15 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   DocumentSnapshot? userData;
 
+  void editProfile() {
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    final User user = auth.currentUser!;
+    showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (ctx) => EditProfile(user: user));
+  }
+
   @override
   void initState() {
     super.initState();
@@ -136,15 +148,84 @@ class _ProfilePageState extends State<ProfilePage> {
     var user = GlobalUserService().currentUser;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('Name: ${user!.displayName}'), // Data from FirebaseAuth
-          Text('Email: ${user.email}'), // Data from FirebaseAuth
-          // Text('Additional Info: ${userData['additional_info']}'), // Data from Firestore
-          // ... more user details
-        ],
-      ),
-    );
+                appBar: AppBar(
+                  title: const Text('Profile'),
+                ),
+                body: Center(
+                  child: ListView(children: [
+                    const SizedBox(height: 30),
+                    ProfileWidget(
+                      imagePath:
+                          'https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D',
+                      onClicked: editProfile,
+                      isEdit: false,
+                    ),
+                    const SizedBox(height: 20),
+                    Text(user!.displayName.toString()),
+                    Text(user!.email.toString()),
+
+                    ElevatedButton(
+                        onPressed: editProfile, child: const Text('Edit'))
+                    //Text(user.school.toString()),
+                  ]
+
+                      /*mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('User Name:'),
+                      Consumer<ProfileViewModel>(
+                        builder: (context, viewModel, child) {
+                          return Text(
+                            viewModel.userModel?.name ?? 'Loading...',
+                            style: const TextStyle(fontSize: 24),
+                          );
+                        },
+                      ),
+                      Builder(
+                        builder: (context) {
+                          return ElevatedButton(
+                            onPressed: () {
+                              Provider.of<ProfileViewModel>(context,
+                                      listen: false)
+                                  .updateUserName('Jane Doe');
+                            },
+                            child: const Text('Change Name'),
+                          );
+                        },
+                      ),
+                      Builder(builder: (context) {
+                        return ElevatedButton(
+                          // Add User Button
+                          onPressed: () {
+                            UserModel newUser = UserModel(
+                              id: user.uid,
+                              name: 'Dummy Name',
+                              email: 'dummy@email.com',
+                              program: 'Dummy Program',
+                              school: 'Dummy School',
+                              year: 'Dummy Year',
+                            );
+                            Provider.of<ProfileViewModel>(context,
+                                    listen: false)
+                                .addUser(newUser);
+                          },
+                          child: const Text('Add User'),
+                        );
+                      }),
+                      Builder(builder: (context) {
+                        return ElevatedButton(
+                          // Delete User Button
+                          onPressed: () {
+                            Provider.of<ProfileViewModel>(context,
+                                    listen: false)
+                                .deleteUser();
+                          },
+                          child: const Text('Delete User'),
+                        );
+                      })
+                    ],*/
+                      ),
+                ),
+              );
   }
 }
 
---
+++ app/lib/features/profile/profile_widget.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class ProfileWidget extends StatelessWidget {
+  const ProfileWidget(
+      {super.key,
+      required this.imagePath,
+      required this.onClicked,
+      required this.isEdit});
+
+  final String imagePath;
+  final VoidCallback onClicked;
+  final bool isEdit;
+
+  Widget buildImage(imagePath) {
+    final image = NetworkImage(imagePath);
+
+    return ClipOval(
+      child: Material(
+        color: Colors.transparent,
+        child: Ink.image(
+          image: image,
+          fit: BoxFit.cover,
+          width: 128,
+          height: 128,
+          child: InkWell(
+            onTap: onClicked,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildEditIcon(Color color) => ClipOval(
+        child: Container(
+          padding: const EdgeInsets.all(2),
+          color: Colors.white,
+          child: ClipOval(
+            child: Container(
+              color: Colors.blue,
+              padding: const EdgeInsets.all(10),
+              child: Icon(
+                isEdit ? Icons.add_a_photo : Icons.edit,
+                size: 20,
+              ),
+            ),
+          ),
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Stack(children: [
+        buildImage(imagePath),
+        Positioned(
+          bottom: 0,
+          right: 4,
+          child: buildEditIcon(Colors.blue),
+        ),
+      ]),
+    );
+  }
+}
